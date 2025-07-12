1ef826e4be4b5508b265c7dee9ae0d8cca9a6c43
daye <myannnng@gmail.com>
2025-07-13 01:18:37 +0900

    React

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -9,19 +9,8 @@
 import "./App.css";
 import { useState } from "react";
 import Button from "./components/Button";
-
-const Bulb = ({ light }) => {
-  console.log(light);
-  return (
-    <div>
-      {light === "ON" ? (
-        <h1 style={{ backgroundColor: "orange" }}>ON</h1>
-      ) : (
-        <h1 style={{ backgroundColor: "gray" }}>OFF</h1>
-      )}
-    </div>
-  );
-};
+import Bulb from "./components/Bulb"
+import Counter from "./components/Counter"
 
 function App() {
   // const buttonProps = {
@@ -32,9 +21,6 @@ function App() {
   //   c : 3,
   // }
 
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-  console.log(count);
 
   return (
     <>
@@ -44,26 +30,9 @@ function App() {
         <Header />
         {/* <div> 자식 요소 </div> */}
       {/* </Button> */}
-      <div>
-        <Bulb light={light} />
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Bulb/>
+      <Counter/>
+
     </>
   );
 }
